c47a45b0dd826532865264b19f80728955219e50
Manuel Sousa <mlrcbsousa@gmail.com>
2020-06-10 23:08:42 +0200

    User interface with polaris

--- .next/static/runtime/webpack.js
+++ .next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "e86ed88aa9587fadf431";
+/******/ 	var hotCurrentHash = "a0c3f0f7064c259d3f17";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
